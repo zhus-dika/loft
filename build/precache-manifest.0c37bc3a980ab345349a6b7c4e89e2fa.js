66b090d7b703120eb40effc673828d86778e9507
zhus-dika <zhus.dinara@gmail.com>
2020-01-25 12:33:51 +0600

    correct socket emits in chat

---
+++ build/precache-manifest.0c37bc3a980ab345349a6b7c4e89e2fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c914213b37f4948e59c4406089578f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72df8e2e6a3b21cd2461",
+    "url": "/static/js/2.3c824360.chunk.js"
+  },
+  {
+    "revision": "7dc514d18ab425f7737c",
+    "url": "/static/js/main.1e256d92.chunk.js"
+  },
+  {
+    "revision": "9ab788a8a754a454583d",
+    "url": "/static/js/runtime~main.7075f839.js"
+  }
+]);
